8379836a6e31bf4ffeeeedfde4139ec28da3f3e0
davidkonrad <davidkonrad@gmail.com>
2022-05-07 11:00:04 +0200

    fix, undefined icon should be rendered properly

--- src/dialogs.js
+++ src/dialogs.js
@@ -99,12 +99,12 @@ const SimplyDialogs = (function(document) {
 		}
 		if (options) parseOptions(use, options)
 		if (type !== 'wait') dialog.querySelector('.dialog-header').innerHTML = use.headers[type]
-		dialog.querySelector('.dialog-icon').innerHTML = use.icons[type]
+		dialog.querySelector('.dialog-icon').innerHTML = use.icons[type] || ''
 		;['ok', 'cancel', 'yes', 'no'].forEach((name) => popBtn(name))
 		if (use.classes && typeof use.classes === 'string') dialog.classList.add(...use.classes.split(' '))
 		if (use.backdrop) {
 			dialog.backdrop = document.createElement('style')
-			dialog.backdrop.id = 'sd' + Math.random()
+			dialog.backdrop.id = 'elvis-has-left-the-building' + Math.random()
 			dialog.backdrop.innerText = `dialog::backdrop { ${use.backdrop.replace(/\r?\n|\r/g, '')} }`
 			document.head.appendChild(dialog.backdrop)
 		}
