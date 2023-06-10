52f890bd339895564d6c669216571adf32b91567
davidkonrad <davidkonrad@gmail.com>
2023-06-11 01:17:17 +0200

    updated .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,6 @@ module.exports = {
 		"root": true,
     "extends": "eslint:recommended",
     "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-				"$": "readonly",
-				"Api": "readonly",
-				"bootbox": "readonly",
-				"hash": "readonly",
-				"dayjs": "readonly",
-				"Crudder": "readonly",
-				"SUNEDITOR": "readonly",
-				"SUNEDITOR_LANG": "readonly",
-				"ACTIVITY": "readonly",
     },
     "parserOptions": {
         "ecmaVersion": "2020",
