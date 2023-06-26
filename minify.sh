033e44aa9d73ae8ad480ec584334680400241954
davidkonrad <davidkonrad@gmail.com>
2023-06-26 06:23:13 +0200

    begin work on themes

--- minify.sh
+++ minify.sh
@@ -2,3 +2,5 @@
 # usage ./minify.sh
 uglifyjs src/dialogs.js --compress --mangle -o dist/dialogs.min.js
 uglifycss src/dialogs.css --output dist/dialogs.min.css
+
+uglifyjs src/themes/bootstrap@5.3.0.js --compress --mangle -o dist/themes/bootstrap@5.3.0.js.min.js
---
+++ src/themes/bootstrap@5.3.0.js
@@ -0,0 +1,30 @@
+/*
+	SimplyDialogs
+	(c) 2023- present David Konrad 
+	https://github.com/davidkonrad/simplydialogs
+	https://simplydialogs.github.io
+
+	Bootstrap 5 theme
+*/
+
+"use strict";
+
+;(function(SimplyDialogs) {
+
+	SimplyDialogs.DEFAULTS.buttons.classes.ok = 'btn btn-primary'
+	SimplyDialogs.DEFAULTS.buttons.classes.cancel = 'btn btn-light'
+	SimplyDialogs.DEFAULTS.buttons.classes.yes = 'btn btn-success'
+	SimplyDialogs.DEFAULTS.buttons.classes.no = 'btn btn-danger'
+
+	SimplyDialogs.DEFAULTS.icons.alert = '<i class="bi-exclamation-triangle-fill text-primary"></i>'
+	SimplyDialogs.DEFAULTS.icons.information = '<i class="bi-info-circle-fill text-primary"></i>'
+	SimplyDialogs.DEFAULTS.icons.confirm = '<i class="bi-question-circle-fill text-primary"></i>'
+	SimplyDialogs.DEFAULTS.icons.bell = '<i class="bi-bell-fill text-primary"></i>'
+	SimplyDialogs.DEFAULTS.icons.error = '<i class="bi-flag-fill text-danger"></i>'
+	SimplyDialogs.DEFAULTS.icons.input = '<i class="bi-pencil-fill text-primary"></i>'
+	SimplyDialogs.DEFAULTS.icons.wait = '<i class="bi-wrench text-secondary"></i>'
+
+	SimplyDialogs.DEFAULTS.input.classes.input = 'form-control'
+	SimplyDialogs.DEFAULTS.input.classes.label = 'col-form-label fw-bolder'
+
+}(SimplyDialogs));
