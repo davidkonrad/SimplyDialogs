8155aaf9a00f0fc6d7fe01f53f4ce5dc8380d216
davidkonrad <davidkonrad@gmail.com>
2023-06-26 06:32:33 +0200

    fix minify.sh

--- minify.sh
+++ minify.sh
@@ -3,4 +3,4 @@
 uglifyjs src/dialogs.js --compress --mangle -o dist/dialogs.min.js
 uglifycss src/dialogs.css --output dist/dialogs.min.css
 
-uglifyjs src/themes/bootstrap@5.3.0.js --compress --mangle -o dist/themes/bootstrap@5.3.0.js.min.js
+uglifyjs src/themes/bootstrap@5.3.0.js --compress --mangle -o dist/themes/bootstrap@5.3.0.min.js
